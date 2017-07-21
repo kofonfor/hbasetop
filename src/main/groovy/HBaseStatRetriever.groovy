7ff307ffd83f05d60b72c7de78f196d0b026524d
Alex Chistyakov <alexclear@gmail.com>
2017-07-21 06:43:17 +0300

    Add a periodic task

---
+++ src/main/groovy/HBaseStatRetriever.groovy
@@ -0,0 +1,55 @@
+import ratpack.exec.ExecController
+import ratpack.service.Service
+import ratpack.service.StartEvent
+import ratpack.service.StopEvent
+import ratpack.exec.Execution
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+import ratpack.http.client.HttpClient
+
+class HBaseStatRetriever implements Service {
+  HttpClient httpClient
+  ScheduledExecutorService executorService
+  volatile ScheduledFuture<?> nextFuture
+  volatile boolean stopped
+
+  HBaseStatRetriever() {
+    print "Retriever started\n"
+  }
+
+  void onStart(StartEvent event) throws Exception {
+    httpClient = event.getRegistry().get(HttpClient.class)
+    executorService = event.getRegistry().get(ExecController.class).getExecutor()
+    stopped = false
+    scheduleNext()
+  }
+
+  void onStop(StopEvent event) throws Exception {
+    stopped = true
+    Optional.ofNullable(nextFuture).ifPresent({ f -> f.cancel(true) })
+  }
+
+  def scheduleNext() {
+    nextFuture = executorService.schedule(this.&run, 30, TimeUnit.SECONDS)
+    print "scheduleNext finished!\n"
+  }
+
+  def run() {
+    if (stopped) {
+      return
+    }
+
+    try {
+      Execution.fork()
+        .onComplete({ e -> print "Complete\n"; scheduleNext() })
+        .onError({ e -> e.printStackTrace() })
+        .start({ e ->
+          httpClient.get(new URI("http://sverka-04:60010/table.jsp?name=invoices_v4")).then({ response ->
+            System.out.println("Status: " + response.getStatusCode()) })
+         })
+    } catch(Exception ex) {
+      println ex
+    }
+  }
+}
--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -2,15 +2,26 @@ import ratpack.groovy.template.MarkupTemplateModule
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
+import ratpack.http.client.StreamedResponse
+import ratpack.http.MutableHeaders
+import ratpack.http.client.RequestSpec
+import ratpack.http.client.HttpClient
 
 ratpack {
   bindings {
+    add new HBaseStatRetriever()
     module MarkupTemplateModule
   }
 
   handlers {
     get {
       render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
+    }
+
+    get("test") { HttpClient httpClient ->
+      httpClient.get(new URI("http://sverka-04:60010/table.jsp?name=invoices_v4")).then({ response ->
+        render "Status: " + response.getStatusCode()
+      })
     }
 
     files { dir "public" }
